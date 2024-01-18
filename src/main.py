7fb85b55f6d36f078e6dd8f9298560e68aaf3042
syasser126 <syasser126@gmail.com>
2024-01-18 17:54:03 -0500

    Finished making the menu. Gonna bench the project now after cleaning up the read me. Made the Rotation menu. Cut the help menu. Moved Cube notation function from menu.py to cube.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,8 @@
 import lib.menu as menu
 from lib.cube import Cube
 
-'''
-Outline for new code:
-
-Ask User to make a new code or exit.
-
-New cube is made.
-
-Ask to preform move, scramble, help, or back.
-
-Preforming move asks user to input move and returns user to previous screen, but locks scrambles.
-
-Preforming scramble will scramble the cube, save the seed, and return user to the previous screen.
-
-Preforming help will give user information about rotations and scramble.
-
-Preforming back will take them back to first screen.
-'''
-
 make_cube = menu.home()
 
 cube = Cube()
 
 menu.cube_menu(cube)
-
-
